e460c41802cd815b52492085743c58d6b003f9b6
John Stuby <johnstuby7@gmail.com>
2023-09-25 10:34:44 -0600

    Finished search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,17 @@
 import React from "react";
 
-
-function Search({ searchInput, search}) {
+function Search({ searchInput, search }) {
   return (
-
-  )
+    <div className="search-bar">
+      <input
+        type="text"
+        placeholder="Search for a Movie..."
+        className="search"
+        onChange={searchInput}
+        onKeyPress={search}
+      />
+    </div>
+  );
 }
 
-export default Search
+export default Search;
