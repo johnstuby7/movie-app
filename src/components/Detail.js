e40be4e65bc56c4944529a2b1e5e2d4b172e476b
John Stuby <johnstuby7@gmail.com>
2023-09-25 10:32:24 -0600

    Added in css for search, finished detail, result and results components

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,10 +1,23 @@
 import React from "react";
 
-
-function Detail({ selected, closeDetail}) {
+function Detail({ selected, closeDetail }) {
   return (
+    <section className="detail">
+      <div className="content">
+        <h2>{selected.Title}</h2>
+      </div>
+      <span>{selected.Year}</span>
+      <p className="rating">Rating: {selected.imdbRating}</p>
 
-  )
+      <div className="about">
+        <img src={selected.Poster} alt="" />
+        <p>{selected.Plot}</p>
+      </div>
+      <button className="close" onClick={closeDetail}>
+        Close
+      </button>
+    </section>
+  );
 }
 
-export default Detail
+export default Detail;
--- src/components/Result.js
+++ src/components/Result.js
@@ -1,10 +1,12 @@
 import React from "react";
 
-
-function Result({ result, openDetail}) {
+function Result({ result, openDetail }) {
   return (
-
-  )
+    <div className="result" onClick={() => openDetail(result.imdbID)}>
+      <img src={result.Poster} />
+      <h3>{result.Title}</h3>
+    </div>
+  );
 }
 
-export default Result
+export default Result;
